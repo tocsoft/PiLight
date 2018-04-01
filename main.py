4c896ee432ff8be14d8fb05fcd29d1c34118667d
Scott Williams <tocsoft@gmail.com>
2018-04-01 20:53:25 +0100

    fix fun name

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def toggleStrobe():
     return 'strobe'
 
 @app.route('/light/bounce')
-def toggleStrobe():
+def toggleBounce():
     lights.setAnimation([[1,0,0,0.5],[0,1,0,0.5],[0,0,1,0.5],[0,1,0,0.5]])
     return 'strobe'
 
