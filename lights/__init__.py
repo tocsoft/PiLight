c5c67b5fb713a9c9b52758c4ffa18e2de0af06a4
Scott Williams <tocsoft@gmail.com>
2018-04-01 14:13:21 +0100

    add lights methods

--- lights/__init__.py
+++ lights/__init__.py
@@ -1,7 +1,59 @@
 
 import automationhat
 import time
+import threading
 
-redLight = 0
-amberLight = 1
-greenLight = 2
+
+_redLight = 0
+_amberLight = 1
+_greenLight = 2
+_thread = threading.Thread(target=_animateLoop, args=())
+
+_offPattern = [[0,0,0]]
+
+_pattern = _offPattern
+_patternIndex = 0 
+_callBack 
+
+def _animateLoop()
+    while True:
+        _patternIndex = _patternIndex % len(_pattern)
+        entry = _pattern[_patternIndex]
+        _setLight(0, entry[0])
+        _setLight(1, entry[1])
+        _setLight(2, entry[2])
+        if(len(entry) == 4):
+            time.sleep(entry[3])
+
+        _patternIndex +=1
+        if(len(_pattern) == _patternIndex & _callBack):
+            if(_callBack()):
+                _patternIndex = 0
+            else:
+                stopAnimation()
+
+def _setLight(light, state):
+    light = light % 3
+    if light == 0:
+        automationhat.output.one.write(state)
+    if light == 1:
+        automationhat.output.two.write(state)
+    if light == 2:
+        automationhat.output.three.write(state)
+
+def allOff():
+    stopAnimation()
+    
+def stopAnimation():
+    _callBack = null
+    _pattern = _offPattern
+    _patternIndex= 0
+    _setLight(0,0)
+    _setLight(1,0)
+    _setLight(2,0)
+    
+def setAnimation(pattern, callBack):
+    stopAnimation()
+    _callBack = callBack
+    _pattern = pattern
+    _patternIndex= 0
