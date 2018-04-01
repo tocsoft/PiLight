16cfdb439af218c9cd75cb2cf272cfbfb4d64150
Scott Williams <tocsoft@gmail.com>
2018-04-01 20:10:11 +0100

    run variable

--- buzzer/__init__.py
+++ buzzer/__init__.py
@@ -13,33 +13,36 @@ _pattern = _offPattern
 _patternIndex = 0 
 _loopCounter = -1
 _callBack = None
+_run = False
 
 def _loop():
     global _patternIndex
+    global _run
     global _pattern
     global _callBack
     global _loopCounter
     while True:
-        if(_patternIndex >= 0):
-            entry = _pattern[_patternIndex]
-            _set(entry[0])
-            if(len(entry) == 2):
-                time.sleep(entry[1])
-            
-            if(_loopCounter > 0):
-                _loopCounter -= 1
-            if(_loopCounter  == 0):
-                stopPattern()
+        if _run:
+            if(_patternIndex >= 0):
+                entry = _pattern[_patternIndex]
+                _set(entry[0])
+                if(len(entry) == 2):
+                    time.sleep(entry[1])
+                
+                if(_loopCounter > 0):
+                    _loopCounter -= 1
+                if(_loopCounter  == 0):
+                    stopPattern()
 
-            _patternIndex +=1
-            if (len(_pattern) == _patternIndex):
-                _patternIndex = 0
-                if(_loopCounter <= 0):
-                    if (_callBack is not None):            
-                        if (_callBack()):
-                            _patternIndex = 0
-                        else:
-                            stopPattern()
+                _patternIndex +=1
+                if (len(_pattern) == _patternIndex):
+                    _patternIndex = 0
+                    if(_loopCounter <= 0):
+                        if (_callBack is not None):            
+                            if (_callBack()):
+                                _patternIndex = 0
+                            else:
+                                stopPattern()
 
 def _set(state, force=False):
     global _currentState
@@ -57,17 +60,18 @@ def stopPattern():
     global _pattern
     global _callBack
     global _loopCounter
+    global _run
     _loopCounter = -1
     _callBack = None
     _patternIndex= -10
     _pattern = _offPattern
-    _patternIndex= 0
-    _set(0)
+    _run = False
     
 def setPattern(pattern, callBack = None, loopCounter = -1):
     global _patternIndex
     global _pattern
     global _loopCounter
+    global _run
     global _callBack
     stopPattern()
     _callBack = callBack
@@ -75,6 +79,7 @@ def setPattern(pattern, callBack = None, loopCounter = -1):
     _loopCounter = loopCounter
     _pattern = pattern
     _patternIndex= 0
+    _run = True
 
 
 _thread = threading.Thread(target=_loop, args=())
